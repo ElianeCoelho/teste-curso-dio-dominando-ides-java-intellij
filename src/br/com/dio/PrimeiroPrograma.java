1064bf97dd88f067252844d263d1ff92083966c1
ElianeCoelho <ellianee@hotmail.com>
2021-10-31 21:24:29 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+/*
+        //sout// inserir o println
+*/
+        //int a = 5;
+        // int b = 3;
+        // System.out.println("Hello World!!! " + (a+b));
+        // Ctrl + Shift + F10 pra rodar o programa */
+    }
+
+}
